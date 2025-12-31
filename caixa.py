7a59f985823a0638a5162a042441bc2b6e7e8575
opropriofabricio <fabricio.mh@outlook.com>
2025-12-30 23:01:34 -0300

    class caixa ED02

--- caixa.py
+++ caixa.py
@@ -66,7 +66,7 @@ class CaixaData():
 
 
 
-    def buscar(self):
+    def buscar(self, uf: Uf):
 
         # Abre o navegador
         self.page = self.google.new_page()
@@ -83,6 +83,46 @@ class CaixaData():
             selector=ElementosId.ID_TIPO_ATENDIMENTO.value,
             value=self.tipo.value,
         )
+
+        # seleciona a unidade federativa
+        self.page.select_option(
+            ElementosId.ID_UF.value, value=uf.value
+            )
+        
+        # seleciona a cidade
+        self.page.select_option(
+            ElementosId.ID_CIDADE.value,
+        )
+
+        # cria lista de cidades
+        self.opcoes = self.page.evaluate(
+        "Array.from(document.querySelectorAll('#ctl00_ctl61_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_ddlCidade option')).map(o => o.value).filter(v => v !== '0');"
+        )
+
+        # loop
+        for cidade in self.opcoes:
+            ...
+
+            # recebe uma cidade
+            self.page.locator(
+            ElementosId.ID_CIDADE.value).select_option(str(cidade))
+            time.sleep(1)
+
+            # clica no botao buscar
+            self.page.click(ElementosId.ID_BOTAO.value)
+            time.sleep(1)
+
+            # verifica se o resultado tem mais paginas dinamicas para serem carregadas
+            status = self.test_botao(self.page, cidade)
+            print(f"Resultado do Botão de ver mais: {status}")
+
+            # loop dados
+            while status:
+                time.sleep(2)
+                status = self.test_botao(self.page, cidade)
+
+            time.sleep(1)
+            
 
         
 
@@ -101,6 +141,17 @@ class CaixaData():
     def ver_salvos(self,):
         ...
 
+    def test_botao(self, obj, op):
+        aux_b = obj.locator(ElementosId.ID_BTN_VER_MAIS.value)
+
+        if aux_b.is_visible():
+            aux_b.click()
+            time.sleep(3)
+
+            return True
+        else:
+            return False
+
 
 
 
@@ -108,5 +159,5 @@ class CaixaData():
 
 with sync_playwright() as plw:
     scrping_rj = CaixaData(plw=plw,tipo=TipoAtendimento.AGENCIAS, uf=Uf.RJ, visivel=False)
-    scrping_rj.buscar()
+    scrping_rj.buscar(uf=Uf.RJ)
     time.sleep(3)
