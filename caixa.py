97fdf472f240327f3ed49f242eea3620652b69be
opropriofabricio <fabricio.mh@outlook.com>
2026-01-01 16:11:51 -0300

    edicao em loterias

--- caixa.py
+++ caixa.py
@@ -5,19 +5,20 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from typing import Literal
 from cleantext import clean
-from datetime import datetime
 
 # em desenvolvimento ***
 
-url_base = 'https://www.caixa.gov.br/atendimento/Paginas/encontre-a-caixa.aspx'
+url_base = "https://www.caixa.gov.br/atendimento/Paginas/encontre-a-caixa.aspx"
+
 
 class ElementosId(StrEnum):
-    ID_TIPO_ATENDIMENTO = '#ctl00_ctl61_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_ddlTipo'
-    ID_UF = '#ctl00_ctl61_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_ddlUf'
-    ID_CIDADE = '#ctl00_ctl61_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_ddlCidade'
-    ID_BOTAO = '#ctl00_ctl61_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_btnBuscar'
-    ID_BTN_VER_MAIS = '#ctl00_ctl61_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_btnVerMais'
-    ID_POP_UP_COOKIE = '#adopt-accept-all-button'
+    ID_TIPO_ATENDIMENTO = "#ctl00_ctl61_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_ddlTipo"
+    ID_UF = "#ctl00_ctl61_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_ddlUf"
+    ID_CIDADE = "#ctl00_ctl61_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_ddlCidade"
+    ID_BOTAO = "#ctl00_ctl61_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_btnBuscar"
+    ID_BTN_VER_MAIS = "#ctl00_ctl61_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_btnVerMais"
+    ID_POP_UP_COOKIE = "#adopt-accept-all-button"
+
 
 class TipoAtendimento(StrEnum):
     AGENCIAS = "1"
@@ -25,51 +26,61 @@ class TipoAtendimento(StrEnum):
     CBANCARIO = "6"
     PATENDIMENTO = "8"
 
+
 class Uf(StrEnum):
-    AC="AC"
-    AL="AL"
-    AP="AP"
-    AM="AM"
-    BA="BA"
-    CE="CE"
-    DF="DF"
-    ES="ES"
-    GO="GO"
-    MA="MA"
-    MT="MT"
-    MS="MS"
-    MG="MG"
-    PA="PA"
-    PB="PB"
-    PR="PR"
-    PE="PE"
-    PI="PI"
-    RJ="RJ"
-    RN="RN"
-    RS="RS"
-    RO="RO"
-    RR="RR"
-    SC="SC"
-    SP="SP"
-    SE="SE"
-    TO="TO"
+    AC = "AC"
+    AL = "AL"
+    AP = "AP"
+    AM = "AM"
+    BA = "BA"
+    CE = "CE"
+    DF = "DF"
+    ES = "ES"
+    GO = "GO"
+    MA = "MA"
+    MT = "MT"
+    MS = "MS"
+    MG = "MG"
+    PA = "PA"
+    PB = "PB"
+    PR = "PR"
+    PE = "PE"
+    PI = "PI"
+    RJ = "RJ"
+    RN = "RN"
+    RS = "RS"
+    RO = "RO"
+    RR = "RR"
+    SC = "SC"
+    SP = "SP"
+    SE = "SE"
+    TO = "TO"
 
 
 class ElementosClasse(StrEnum):
     RESULTADO_BUSCA_ITEM = ".resultado-busca-item"
+    H4 = "h4.resultado-busca-titulo"
+    ENDERECO = "h4.resultado-busca-titulo + p"
+    CGC = "span.resultado-busca-agencia"
+    ATIVIDADE = "p.informacoes + p"
 
 
-class CaixaData():
+class CaixaData:
 
-    def __init__(self,*, plw: Playwright, tipo: TipoAtendimento, uf: Uf, visivel: Literal[True, False]) -> None:
+    def __init__(
+        self,
+        *,
+        plw: Playwright,
+        tipo: TipoAtendimento,
+        uf: Uf,
+        visivel: Literal[True, False],
+    ) -> None:
         self.data = []
         self.data_agencia = []
+        self.data_loteria = []
         self.tipo = tipo
         self.uf = uf
         self.google = plw.chromium.launch(headless=visivel)
-
-
-
 
     def buscar(self, uf: Uf):
 
@@ -82,7 +93,6 @@ class CaixaData():
         # fecha o pop up
         self.page.click(ElementosId.ID_POP_UP_COOKIE.value)
 
-
         # seleciona o tipo de atendimento
         self.page.select_option(
             selector=ElementosId.ID_TIPO_ATENDIMENTO.value,
@@ -90,10 +100,8 @@ class CaixaData():
         )
 
         # seleciona a unidade federativa
-        self.page.select_option(
-            ElementosId.ID_UF.value, value=self.uf
-            )
-        
+        self.page.select_option(ElementosId.ID_UF.value, value=self.uf)
+
         # seleciona a cidade
         self.page.select_option(
             ElementosId.ID_CIDADE.value,
@@ -101,15 +109,14 @@ class CaixaData():
 
         # cria lista de cidades
         self.opcoes = self.page.evaluate(
-        "Array.from(document.querySelectorAll('#ctl00_ctl61_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_ddlCidade option')).map(o => o.value).filter(v => v !== '0');"
+            "Array.from(document.querySelectorAll('#ctl00_ctl61_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_ddlCidade option')).map(o => o.value).filter(v => v !== '0');"
         )
 
         # loop
-        for cidade in self.opcoes[:10]: #reduzir loop para teste
+        for cidade in self.opcoes[:5]:  # reduzir loop para teste
 
             # recebe uma cidade
-            self.page.locator(
-            ElementosId.ID_CIDADE.value).select_option(cidade)
+            self.page.locator(ElementosId.ID_CIDADE.value).select_option(cidade)
             time.sleep(1)
 
             # clica no botao buscar
@@ -134,26 +141,46 @@ class CaixaData():
             itens = soup.select(ElementosClasse.RESULTADO_BUSCA_ITEM.value)
 
             for i, resultado in enumerate(itens):
-                
+
                 if self.tipo == TipoAtendimento.AGENCIAS:
-                    agencia = resultado.select("h4.resultado-busca-titulo")[0].text
-                    endereco_aux = resultado.select("h4.resultado-busca-titulo + p")[0].text
-                    cgc = resultado.select("span.resultado-busca-agencia")[0].text
-                    endereco = clean(endereco_aux, normalize_whitespace=True, no_line_breaks=True)
+                    agencia = resultado.select(ElementosClasse.H4.value)[0].text
+                    endereco_aux = resultado.select(ElementosClasse.ENDERECO.value)[
+                        0
+                    ].text
+                    cgc = resultado.select(ElementosClasse.CGC.value)[0].text
+                    endereco = clean(
+                        endereco_aux, normalize_whitespace=True, no_line_breaks=True
+                    )
                     linha = [agencia, cgc, endereco]
                     self.data_agencia.append(linha)
-                    
 
                 elif self.tipo == TipoAtendimento.LOTERIAS:
-                    nome_fantasia = resultado.select("h4.resultado-busca-titulo").text
-                    print(nome_fantasia)
-                    razao_social = ...
-                    endereco = ...
-                    cnpj = ...
-                    ag_vinculada = ...
-                    email = ...
-                    atividade = ...
+                    nome_fantasia = resultado.select(ElementosClasse.H4.value)[0].text
+                    razao_social = resultado.find(
+                        "b", string="Razão Social:"
+                    ).next_sibling
+                    cnpj = resultado.find("b", string="CNPJ:").next_sibling
+                    ag_vinculada = resultado.find(
+                        "b", string="Agência de vinculação:"
+                    ).next_sibling
+                    email = resultado.find("b", string="E-mail:").next_sibling
+                    endereco_loteria = resultado.select(ElementosClasse.ENDERECO.value)[
+                        0
+                    ].text.strip()
+                    atividade = resultado.select(ElementosClasse.ATIVIDADE.value)[
+                        0
+                    ].text.strip()
 
+                    linha_loteria = [
+                        nome_fantasia,
+                        razao_social,
+                        cnpj,
+                        ag_vinculada,
+                        email,
+                        endereco_loteria,
+                        atividade,
+                    ]
+                    self.data_loteria.append(linha_loteria)
 
                 elif self.tipo == TipoAtendimento.CBANCARIO:
                     ...
@@ -161,25 +188,33 @@ class CaixaData():
                 elif self.tipo == TipoAtendimento.PATENDIMENTO:
                     ...
 
-
-
-
-        
-
-
-
-        
-        return print('Finalizado!')
-
-    
+        return print("Finalizado!")
 
     def exportar(self):
-        df = pd.DataFrame(self.data_agencia, columns=['Nome', 'CGC', 'Endereco'])
-        data = datetime.now()
-        df.to_csv(f'./{self.tipo.value}_{self.uf.value}.csv')
+        if self.tipo == TipoAtendimento.AGENCIAS:
+            df = pd.DataFrame(self.data_agencia, columns=["Nome", "CGC", "Endereco"])
+            df.to_csv(f"./Agencias/Agencias_{self.uf.value}.csv")
+            print("Arquivo exportado!")
 
-    def ver_salvos(self,):
-        ...
+        elif self.tipo == TipoAtendimento.LOTERIAS:
+            df = pd.DataFrame(
+                self.data_loteria,
+                columns=[
+                    "Nome Fantasia",
+                    "Razao Social",
+                    "CNPJ",
+                    "Agencia Vinculada",
+                    "Email",
+                    "Endereco",
+                    "Atividade",
+                ],
+            )
+            df.to_csv(f"./Loterias/Loterias_{self.uf.value}.csv")
+            print("Arquivo exportado!")
+
+    def ver_salvos(
+        self,
+    ): ...
 
     def test_botao(self, obj, op):
         aux_b = obj.locator(ElementosId.ID_BTN_VER_MAIS.value)
@@ -193,12 +228,10 @@ class CaixaData():
             return False
 
 
-
-
-
-
 with sync_playwright() as plw:
-    scrping_rj = CaixaData(plw=plw,tipo=TipoAtendimento.AGENCIAS, uf=Uf.SP, visivel=False)
-    scrping_rj.buscar(uf=Uf.RJ)
+    scrping_rj = CaixaData(
+        plw=plw, tipo=TipoAtendimento.LOTERIAS, uf=Uf.ES, visivel=False
+    )
+    scrping_rj.buscar(uf=Uf.ES)
     time.sleep(3)
     scrping_rj.exportar()
